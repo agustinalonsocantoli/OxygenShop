e0634e1b79ad57d89a90d49a8422ce90953bd251
agustinalonsocantoli <agustinalonsocantoli@gmail.com>
2023-01-10 19:22:08 +0100

    Avances en PopUp, envio de formulario y subscripcion al servidor

--- src/js/index.js
+++ src/js/index.js
@@ -29,9 +29,35 @@ window.addEventListener('load', () => {
         //Calcular el porcentaje que el usuario ha scrolleado y mostrarlo
         let scrollTop = document.documentElement['scrollTop'] || document.body['scrollTop'];
         let scrollBottom = (document.documentElement['scrollHeight'] || document.body['scrollHeight']) - document.documentElement.clientHeight;
-        let scrollPercent = (scrollTop / scrollBottom * 100).toFixed(0);
+        let scrollPercent = parseFloat((scrollTop / scrollBottom * 100).toFixed(0));
 
         percent.innerHTML = `${scrollPercent}%`;
+
+        if(scrollPercent > 25) {
+            popup.style.display = 'block';
+        }
+    });
+
+    //POPUP "SUBSCRIBE TO OUR NEWSLETTER"
+    let popup = document.querySelector('.popup');
+    let cerrarPopup = document.querySelector('.popup__icon');
+
+    setTimeout(function() {
+        popup.style.display = 'block';
+    }, 5000)
+
+    document.querySelector('main').addEventListener('click', () => {
+        popup.style.display = 'none';
+    })
+
+    cerrarPopup.addEventListener('click', () => {
+        popup.style.display = 'none';
+    })
+
+    document.addEventListener('keydown', function(event) {
+        if(event.key == 'Escape') {
+            popup.style.display = 'none';
+        }
     });
 
     // BOTON RETURN TO THE TOP
@@ -44,7 +70,7 @@ window.addEventListener('load', () => {
         }, 200);
     });
 
-    // VALIDAR FORMULARIO
+    // VALIDAR FORMULARIO, RECOGER DATOS Y ENVIAR
     let name = document.querySelector('#name');
     let email = document.querySelector('#email');
     let check = document.querySelector('#check');
@@ -52,19 +78,73 @@ window.addEventListener('load', () => {
     let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     form.addEventListener('submit', (e) => {
+        let contact = {
+            name,
+            email,
+        }
+
         if(name.value.length < 2 || name.value.length > 100 && !isNaN(name.value)) {
             e.preventDefault();
             name.style.border = '1px solid red';
-        }
+        } else {
+            contact.name = name.value;
 
-        if(!email.value.match(regex)) {
-            e.preventDefault();
-            email.style.border = '1px solid red';
-        }
+            if(!email.value.match(regex)) {
+                e.preventDefault();
+                email.style.border = '1px solid red';
+            } else {
+                contact.email = email.value;
 
-        if (!check.checked) {
+                if (!check.checked) {
+                    e.preventDefault();
+                    document.querySelector('.checkbox__container').style.border = '1px solid red';
+                } else {
+                    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+                        method: 'PUT',
+                        body: JSON.stringify({
+                            name: contact.name,
+                            email: contact.email
+                        }),
+                        headers: {
+                            'Content-type': 'application/json; charset=UTF-8',
+                        },
+                        })
+                        .then((response) => response.json())
+                        .then((json) => console.log(json));
+
+                        form.reset();
+                }
+
+            }
+        }
+    });
+
+    // VALIDAR POPUP, RECOGER DATOS Y ENVIAR
+    let emailPopup = document.querySelector('.popup__input');
+    let formPopup = document.querySelector('.form__popup');
+
+    formPopup.addEventListener('submit', (e) => {
+        let contactPopup;
+
+        if(!emailPopup.value.match(regex)) {
             e.preventDefault();
-            document.querySelector('.checkbox__container').style.border = '1px solid red';
+            emailPopup.style.border = '2px solid red';
+        } else {
+            contactPopup = emailPopup.value;
+
+            fetch('https://jsonplaceholder.typicode.com/posts/1', {
+                method: 'PUT',
+                body: JSON.stringify({
+                    email: contactPopup
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+                })
+                .then((response) => response.json())
+                .then((json) => console.log(json));
+        
+                formPopup.reset();
         }
     });
 
@@ -123,7 +203,4 @@ window.addEventListener('load', () => {
     };
 
     new Slider('#slider');
-
-    //POPUP "SUBSCRIBE TO OUR NEWSLETTER"
-
  });
