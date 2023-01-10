441380dcf42a9dca4bcd94a942803ddc717a67c1
agustinalonsocantoli <agustinalonsocantoli@gmail.com>
2023-01-10 15:39:21 +0100

    Modulo JS en proceso. Terminado:  Slide, Scroll, Return to the top, Menu mobile, validar formulario

---
+++ src/js/index.js
@@ -0,0 +1,129 @@
+'use strict'
+
+window.addEventListener('load', () => {
+
+    //MENU HAMBURGUESA PARA MOBILE
+    let desplegar = document.querySelector('.desplegar');
+    let cerrar = document.querySelector('.cerrar');
+    let menu = document.querySelector('.navegacion__lista');
+
+    cerrar.style.display = 'none'
+
+    desplegar.addEventListener('click', () => {
+        console.log('Haz dado click');
+        menu.style.display = 'block';
+        desplegar.style.display = 'none';
+        cerrar.style.display = 'block'
+    });
+
+    cerrar.addEventListener('click', () => {
+        menu.style.display = 'none';
+        desplegar.style.display = 'block';
+        cerrar.style.display = 'none';
+    });
+
+    // PORCENTAJE SCROLL
+    let percent = document.querySelector('.percent');
+
+    window.addEventListener('scroll',() => {
+        //Calcular el porcentaje que el usuario ha scrolleado y mostrarlo
+        let scrollTop = document.documentElement['scrollTop'] || document.body['scrollTop'];
+        let scrollBottom = (document.documentElement['scrollHeight'] || document.body['scrollHeight']) - document.documentElement.clientHeight;
+        let scrollPercent = (scrollTop / scrollBottom * 100).toFixed(0);
+
+        percent.innerHTML = `${scrollPercent}%`;
+    });
+
+    // BOTON RETURN TO THE TOP
+    let returnTop = document.querySelector('.btn__return');
+
+
+    returnTop.addEventListener('click', () => {
+        setTimeout(function() {
+            window.scrollTo(0, 0)
+        }, 200);
+    });
+
+    // VALIDAR FORMULARIO
+    let name = document.querySelector('#name');
+    let email = document.querySelector('#email');
+    let check = document.querySelector('#check');
+    let form = document.querySelector('#form');
+    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    form.addEventListener('submit', (e) => {
+        if(name.value.length < 2 || name.value.length > 100 && !isNaN(name.value)) {
+            e.preventDefault();
+            name.style.border = '1px solid red';
+        }
+
+        if(!email.value.match(regex)) {
+            e.preventDefault();
+            email.style.border = '1px solid red';
+        }
+
+        if (!check.checked) {
+            e.preventDefault();
+            document.querySelector('.checkbox__container').style.border = '1px solid red';
+        }
+    });
+
+    // SLIDER DE IMAGENES
+    class Slider {
+        constructor(sliderSelector) {
+            this.slider = document.querySelector(sliderSelector);
+            this.slidesImg = document.querySelectorAll('.slider__container img')
+            this.sliderContainer = document.querySelector('.slider__container');
+            this.nextBtn = document.querySelector('.next');
+            this.prevBtn = document.querySelector('.prev');
+            this.slideSize = this.slider.offsetWidth;
+            this.countSlide = 0;
+    
+            this.setEventListeners();
+            this.generateDots();
+        };
+
+        setEventListeners(){
+            this.nextBtn.addEventListener('click', () => {
+                if(this.countSlide != this.slidesImg.length - 1){
+                    this.countSlide = this.countSlide >= this.slidesImg -1 ? 0 : this.countSlide + 1;
+                    this.sliderContainer.style.transform = `translateX(-${this.countSlide * this.slideSize}px)`;
+                };
+            });
+        
+            this.prevBtn.addEventListener('click', () => {
+                
+                if(this.countSlide != 0){
+                    this.countSlide = this.countSlide <= 0 ? this.slidesImg - 1 : this.countSlide - 1;
+                    this.sliderContainer.style.transform = `translateX(-${this.countSlide * this.slideSize}px)`;
+                };
+            });
+        };
+
+        generateDots() {
+            const dots = document.createElement('div');
+            dots.classList.add('dots');
+
+            for(let i = 0; i < this.slidesImg.length; i++) {
+
+                const dot = document.createElement('span');
+
+                dot.addEventListener('click', () => {
+                    this.countSlide = i;
+                    this.sliderContainer.style.transform = `translateX(-${this.countSlide * this.slideSize}px)`;
+                });
+
+                dot.classList.add('dot');
+                dots.appendChild(dot);
+            };
+
+            this.slider.appendChild(dots);
+            this.dots = dots;
+        };
+    };
+
+    new Slider('#slider');
+
+    //POPUP "SUBSCRIBE TO OUR NEWSLETTER"
+
+ });
